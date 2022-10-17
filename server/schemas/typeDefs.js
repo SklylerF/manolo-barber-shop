9b3264f053c6131df9770d6f54addf23f06044e6
JM <georgemares14@yahoo.com>
2022-10-16 20:48:38 -0700

    Working on appoingment typeDef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,14 +6,37 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
   type User {
     _id: ID!
+    username: String!
     name: String!
     email: String!
     password: String!
   }
 
+  type Appointment {
+    _id: ID!
+    user: User!
+  }
+
+  type AppointmentTime {
+  time: String!
+  appointments: [Appointment!]!
+}
+
+type Day {
+  # The Day ID could be the actual day itself, i.e. 2020-12-29
+  id: ID!
+  open: Boolean!
+  hours: Int!
+  appointmentTimes: AppointmentTime!
+}
+
+type Schedule {
+ days: [Day!]!
+}
+
   type Query {
     Users: [User]!
-    User(UserId: ID!): User
+    singleUser(UserId: ID!): User
   }
 
   type Auth {
@@ -21,8 +44,8 @@ const typeDefs = gql`
     User: User
   }
   type Mutation {
-    addUser(name: String!): User
-    removeUser: User
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
   }
 `;
 
