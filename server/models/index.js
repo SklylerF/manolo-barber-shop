8c226919afa62683ba8cb2a7049d74448ed4b493
JM <georgemares14@yahoo.com>
2022-10-18 20:24:40 -0700

    added order and product models and imported into index

--- server/models/index.js
+++ server/models/index.js
@@ -2,5 +2,8 @@
 const User = require('./User');
 const Appointment = require('./Appointment');
 const DAY = require('./Day')
+const Product = require('./Product');
+const Category = require('./Category');
+const Order = require('./Order');
 /* Exporting models. */
 module.exports = { User , Appointment , DAY };
