3a6ad3e362443e65bf8407c6f94b5a0d8f6075bc
Cruz Cordero Leon <105164264+CorderoCruz@users.noreply.github.com>
2022-10-21 17:20:52 -0700

    added footer with clickable content and google map using google maps api

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -3,12 +3,13 @@ import FirstSlide from "./first-slide/FirstSlide";
 import ImageSlider from "./carousel/ImageSlider";
 import AboutUs from "./AboutUs/AboutUs";
 import OurTeam from "./OurTeam/OurTeam";
+import Footer from "./Footer/Footer";
+import "../App.css";
 import { SliderData } from "./carousel/SliderData.js";
 
 const styles = {
+  zIndex: 10000,
   backgroundColor: "black",
-  height: "100vh",
-  width: "100vw",
 };
 
 export default function LandingPage() {
@@ -26,11 +27,10 @@ export default function LandingPage() {
       <section>
         <OurTeam />
       </section>
-      <section>
-        <div>
-          <ImageSlider slides={SliderData} />;
-        </div>
-      </section>
+      <ImageSlider slides={SliderData} />;
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 }
