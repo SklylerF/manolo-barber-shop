f29e8c123ba93311ebea8b78ac638d4a1cb25b60
Cruz Cordero Leon <105164264+CorderoCruz@users.noreply.github.com>
2022-10-24 00:42:22 -0700

    added flippable card transition and updated footer with company's socials

--- client/src/components/Shop.js
+++ client/src/components/Shop.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Navbar from "./navbar/Navbar";
 import Footer from "./Footer/Footer";
-import Cards from "./CardFlip/Cards";
+import CardList from "./Cards/CardList";
 //importing product images
 import ProductOne from "../assets/images/product-one.jpg";
 import ProductTwo from "../assets/images/product-two.jpg";
@@ -9,14 +9,18 @@ import ProductTwo from "../assets/images/product-two.jpg";
 export default function Shop() {
   const shopItems = [
     {
-      name: "Item 1",
-      category: "Hair",
+      id: 1,
+      name: "Kalive Urban Pomade for Men",
+      description:
+        "Kalive Pomade will give your hair all the control and flexibility needed to easily create your desired look all while nourishing your hair with natural ingredients. Our premium water-based pomade can be washed out easily, won’t damage your hair and won’t clog your pores.",
       price: 13.99,
       image: ProductOne,
     },
     {
-      name: "Item 2",
-      category: "Hair",
+      id: 2,
+      name: "Kalive2style Mens Hair Styling Gel",
+      description:
+        "Our styling hair gel is made to offer a strong hold to give you the freedom to tame your hair and get it to look just the way you want while adding just a touch of shine",
       price: 13.99,
       image: ProductTwo,
     },
@@ -27,10 +31,11 @@ export default function Shop() {
       <div>
         <Navbar />
       </div>
+      <div className='item-shop'>
+        <h2>SHOP</h2>
+        <CardList shopItems={shopItems} />
+      </div>
       <div>
-        <div>
-          <Cards shopItems={shopItems} />
-        </div>
         <Footer />
       </div>
     </div>
--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -20,8 +20,10 @@ export default function Navbar() {
   return (
     <div className='navbar-container'>
       <header className='header-container'>
-        <img className='manolo-logo' src={ManoloLogo} alt='Manolo Logo' />
-        <h2>MANOLO BARBERSHOP</h2>
+        <Link to='/'>
+          <img className='manolo-logo' src={ManoloLogo} alt='Manolo Logo' />
+          <h2>MANOLO BARBERSHOP</h2>
+        </Link>
         <nav ref={navRef}>
           <img className='manolo-logo-nav' src={ManoloLogo} alt='Manolo Logo' />
           <div className='nav-text'>
