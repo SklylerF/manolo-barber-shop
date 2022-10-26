8012f215ccb5c3b9a01c484e9e15c8bea61ea9d7
Cruz Cordero Leon <105164264+CorderoCruz@users.noreply.github.com>
2022-10-25 17:06:32 -0700

    deloy

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 
 /* This is connecting to the database. */
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/ManoloBarbershop`, {
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/Manolo-Barbershop`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
