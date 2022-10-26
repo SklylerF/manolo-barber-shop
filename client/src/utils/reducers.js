06c7861eb43ff99948bba0b176a69cbd433dd1c4
JM <georgemares14@yahoo.com>
2022-10-26 11:09:02 -0700

    Working on cart

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,9 @@
+import { useReducer } from "react";
+import {
+  ADD_TO_CART,
+  UPDATE_CART_QUANTITY,
+  REMOVE_FROM_CART,
+  ADD_MULTIPLE_TO_CART,
+  CLEAR_CART,
+  TOGGLE_CART
+} from "./actions";
