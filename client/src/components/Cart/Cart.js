24078c07773207b718d188b6b2ff78456fa5e3e9
JM <georgemares14@yahoo.com>
2022-10-26 12:41:40 -0700

    Working on cart

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -3,13 +3,13 @@ import { loadStripe } from '@stripe/stripe-js';
 import { useLazyQuery } from '@apollo/client';
 import { QUERY_CHECKOUT } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
-import CartItem from '../CartItem';//TODO
+import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import './style.css';
 
-const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx'); //TODO
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
