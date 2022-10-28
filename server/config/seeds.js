a3f374af373c680ce22111a3bd9d665107f5c40f
JM <georgemares14@yahoo.com>
2022-10-27 19:24:13 -0700

    renamed images

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -17,7 +17,7 @@ db.once('open', async () => {
       name: 'Kalive Urban Pomade for Men',
       description:
         'Kalive Pomade will give your hair all the control and flexibility needed to easily create your desired look all while nourishing your hair with natural ingredients. Our premium water-based pomade can be washed out easily, won’t damage your hair and won’t clog your pores.',
-      image: 'product-one.jpg',
+      image: 'pproduct-one.68ddf189d137c495abda.jpg',
       category: categories[0]._id,
       price: 13.99,
       quantity: 10000
@@ -26,7 +26,7 @@ db.once('open', async () => {
       name: 'Kalive2style Mens Hair Styling Gel',
       description:
         'Our styling hair gel is made to offer a strong hold to give you the freedom to tame your hair and get it to look just the way you want while adding just a touch of shine',
-      image: 'product-two.jpg',
+      image: 'product-two.20ab04f8c73f3e9429b2.jpg',
       category: categories[0]._id,
       price: 13.99,
       quantity: 10000
