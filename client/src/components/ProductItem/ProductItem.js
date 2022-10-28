3757edd0bf9097e389fc2779d2b6eea9c181dc9c
Cruz Cordero Leon <105164264+CorderoCruz@users.noreply.github.com>
2022-10-27 21:19:28 -0700

    added css styling to cart page

--- client/src/components/ProductItem/ProductItem.js
+++ client/src/components/ProductItem/ProductItem.js
@@ -1,58 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { pluralize } from "../../utils/helpers"
+import { pluralize } from "../../utils/helpers";
 import { useStoreContext } from "../../utils/GlobalState";
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
-import "../../components/Cards/cards.css"
+import "../../components/Cards/cards.css";
 function ProductItem(item) {
   const [state, dispatch] = useStoreContext();
 
-  const {
-    image,
-    name,
-    _id,
-    price,
-    quantity
-  } = item;
+  const [flip, setFlip] = useState(false);
 
-  const { cart } = state
+  const { image, name, _id, price, quantity, description } = item;
+
+  const { cart } = state;
 
   const addToCart = () => {
-    const itemInCart = cart.find((cartItem) => cartItem._id === _id)
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id);
     if (itemInCart) {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: _id,
-        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
       });
-      idbPromise('cart', 'put', {
+      idbPromise("cart", "put", {
         ...itemInCart,
-        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
       });
     } else {
       dispatch({
         type: ADD_TO_CART,
-        product: { ...item, purchaseQuantity: 1 }
+        product: { ...item, purchaseQuantity: 1 },
       });
-      idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
+      idbPromise("cart", "put", { ...item, purchaseQuantity: 1 });
     }
-  }
+  };
 
   return (
-    <div className="card px-1 py-1">
-      <Link to={`/products/${_id}`}>
-        <img
-          alt={name}
-          src={`/static/media/${image}`}
-        />
-        <p>{name}</p>
-      </Link>
-      <div>
-        <div>{quantity} {pluralize("item", quantity)} in stock</div>
-        <span>${price}</span>
+    <div className={`cards ${flip ? "flip" : ""}`} onClick={() => setFlip(!flip)}>
+      <div className='card-back'>
+        <h2>{name}</h2>
+        <p>{description}</p>
       </div>
-      <button onClick={addToCart}>Add to cart</button>
+      <div className='card-front'>
+        <h2>{name}</h2>
+        <h2>${price}</h2>
+        <img src={`static/media/${image}`} />
+        <button onClick={addToCart}>ADD TO CART</button>
+      </div>
     </div>
   );
 }
--- client/src/components/ProductList/ProductList.js
+++ client/src/components/ProductList/ProductList.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react';
-import ProductItem from '../ProductItem/ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
-import { useQuery } from '@apollo/client';
-import { QUERY_PRODUCTS } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
-import spinner from '../../assets/spinner.gif';
+import React, { useEffect } from "react";
+import ProductItem from "../ProductItem/ProductItem";
+import { useStoreContext } from "../../utils/GlobalState";
+import { UPDATE_PRODUCTS } from "../../utils/actions";
+import { useQuery } from "@apollo/client";
+import { QUERY_PRODUCTS } from "../../utils/queries";
+import { idbPromise } from "../../utils/helpers";
+import spinner from "../../assets/spinner.gif";
 
 function ProductList() {
   const [state, dispatch] = useStoreContext();
@@ -21,10 +21,10 @@ function ProductList() {
         products: data.products,
       });
       data.products.forEach((product) => {
-        idbPromise('products', 'put', product);
+        idbPromise("products", "put", product);
       });
     } else if (!loading) {
-      idbPromise('products', 'get').then((products) => {
+      idbPromise("products", "get").then((products) => {
         dispatch({
           type: UPDATE_PRODUCTS,
           products: products,
@@ -38,16 +38,14 @@ function ProductList() {
       return state.products;
     }
 
-    return state.products.filter(
-      (product) => product.category._id === currentCategory
-    );
+    return state.products.filter((product) => product.category._id === currentCategory);
   }
 
   return (
-    <div className="my-2">
-      <h2>Our Products:</h2>
+    <div className='my-2'>
+      <h2>SHOP</h2>
       {state.products.length ? (
-        <div className="flex-row">
+        <div className='flex-row'>
           {filterProducts().map((product) => (
             <ProductItem
               key={product._id}
@@ -56,13 +54,14 @@ function ProductList() {
               name={product.name}
               price={product.price}
               quantity={product.quantity}
+              description={product.description}
             />
           ))}
         </div>
       ) : (
         <h3>You haven't added any products yet!</h3>
       )}
-      {loading ? <img src={spinner} alt="loading" /> : null}
+      {loading ? <img src={spinner} alt='loading' /> : null}
     </div>
   );
 }
